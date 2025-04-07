67ff62f40625266eee12f95ac3455f1f01d0e4df
hliaskostor <73614078+hliaskostor@users.noreply.github.com>
2025-04-07 12:50:58 +0300

    new

--- src/main/java/com/example/register/repositories/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.register.repositories;
-
-import com.example.register.models.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
